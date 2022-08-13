763a42b723fb4ce2d1e5bf9bfdc9be3fb459ae3c
altinawiwaseem <wasemm@live.com>
2022-08-13 14:11:21 +0200

    fetching countries data  and apply it to the infoBoxes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import InfoBox from "./components/InfoBox/InfoBox";
 import Map from "./components/Map/Map";
 
 function App() {
-  const { countries } = useContext(CountriesFetchingContext);
+  // Context
+  const { countries, tableData, countryInfo, setCountryInfo } = useContext(
+    CountriesFetchingContext
+  );
+
+  // State
   const [country, setCountry] = useState("All Countries");
 
-  const handleCountryChange = (e) => {
+  // onChange function for choosing the country to show its data
+  const handleCountryChange = async (e) => {
     const countryCode = e.target.value;
-    console.log(countryCode);
     setCountry(countryCode);
+    const URL =
+      countryCode === "All Countries"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    setCountryInfo(data);
   };
 
   return (
@@ -22,7 +36,7 @@ function App() {
           <h1>COVID-19 TRACKER</h1>
           <form>
             <select value={country} onChange={handleCountryChange}>
-              <option value={"all-countries"}>{country}</option>
+              <option value={"All Countries"}>All Countries</option>
               {countries.map((country, i) => (
                 <option value={country.countryInfo.iso2} key={i}>
                   {country.country}
@@ -32,11 +46,23 @@ function App() {
           </form>
         </div>
 
-        {/* InfoBoxs */}
+        {/* InfoBoxes */}
         <div className="infoBoxes">
-          <InfoBox title="Corona Virus Cases" total={2000} cases={555} />
-          <InfoBox title="Recovered" total={20} cases={55445} />
-          <InfoBox title="Death" total={200220} cases={65} />
+          <InfoBox
+            title="Corona Virus Cases"
+            total={countryInfo.cases}
+            cases={countryInfo.todayCases}
+          />
+          <InfoBox
+            title="Recovered"
+            total={countryInfo.recovered}
+            cases={countryInfo.todayRecovered}
+          />
+          <InfoBox
+            title="Death"
+            total={countryInfo.deaths}
+            cases={countryInfo.todayDeaths}
+          />
         </div>
 
         {/* Map */}
@@ -47,9 +73,8 @@ function App() {
         {/* table */}
         <div>
           <h3>Live Cases by Country</h3>
-        </div>
-        {/* Graph */}
-        <div>
+          <table countries={tableData} />
+
           <h3> Worldwide New Cases</h3>
         </div>
       </div>
--- src/context/CountriesFetching/CountriesFetching.jsx
+++ src/context/CountriesFetching/CountriesFetching.jsx
@@ -4,22 +4,55 @@ export const CountriesFetchingContext = createContext();
 
 let initialData = [];
 
-const URL = "https://disease.sh/v3/covid-19/countries";
+const COUNTRIES_URL = "https://disease.sh/v3/covid-19/countries";
 
+const COUNTRIES_ALL_DATA = "https://disease.sh/v3/covid-19/all";
+
+// fetching countries
 const countriesFetching = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
   return data;
 };
 
+// fetching All the Countries data
+
+const allCountriesData = async () => {
+  const response = await fetch(COUNTRIES_ALL_DATA);
+  const data = await response.json();
+  return data;
+};
+
 export const CountriesFetchingContextProvider = ({ children }) => {
+  // useState
   const [countries, setCountries] = useState(initialData);
+  const [tableData, setTableData] = useState(initialData);
+  const [countryInfo, setCountryInfo] = useState({});
 
   useEffect(() => {
-    countriesFetching(URL).then((data) => setCountries(data));
+    countriesFetching(COUNTRIES_URL).then((data) => {
+      setCountries(data);
+      setTableData(data);
+    });
+  }, []);
+
+  // useEffect for the all countries data
+
+  useEffect(() => {
+    allCountriesData().then((data) => {
+      setCountryInfo(data);
+    });
   }, []);
   return (
-    <CountriesFetchingContext.Provider value={{ countries, setCountries }}>
+    <CountriesFetchingContext.Provider
+      value={{
+        countries,
+        setCountries,
+        tableData,
+        countryInfo,
+        setCountryInfo,
+      }}
+    >
       {children}
     </CountriesFetchingContext.Provider>
   );
